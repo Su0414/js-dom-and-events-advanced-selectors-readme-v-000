d8e5a6b957eb4b8b1596604f811e2c352c8234ed
Suchitra Naik <susanaik@gmail.com>
2018-07-14 20:54:15 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -14,7 +14,7 @@ function ninjaBabySelector(){
 function divSelector(){
   return $('.pics')
 }
-//
-// function firstListItem(){
-//
-// }
+
+function firstListItem(){
+  return $('ul li:first-child')
+}
